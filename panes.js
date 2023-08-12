175584078220085e1da5b9c26038857d15898c0e
Glitch (glitch-hello-website) <none>
2023-08-12 10:19:04 +0000

    🐝🍎 Checkpoint ./panes.js:57276184/429

--- panes.js
+++ panes.js
@@ -1,2 +1,19 @@
 const panes = document.querySelectorAll(".pane")
 
+let z = 1
+
+panes.forEach(pane => {
+  const title = pane.querySelector('.title')
+  
+  pane.addEventListener("mousedown", () => {
+    z = z + 1
+    pane.style.zIndex = z
+  })
+  
+  title.addEventListener('mousedown', ()  => {
+    pane.classList.add('is-dragging')
+    
+    
+  })
+  
+})
