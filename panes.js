2e663b84ade95a51edd92d56bbe028dd94813ad4
Glitch (glitch-hello-website) <none>
2023-08-12 10:30:19 +0000

    🏠🛍 Checkpoint ./panes.js:57276184/807

--- panes.js
+++ panes.js
@@ -10,10 +10,30 @@ panes.forEach(pane => {
     pane.style.zIndex = z
   })
   
-  title.addEventListener('mousedown', ()  => {
+  title.addEventListener('mousedown', (event)  => {
     pane.classList.add('is-dragging')
     
+    let l = pane.offsetLeft
+    let t = pane.offsetTop
     
+    let startX = event.pageX
+    let startY = event.pageY
+    
+    const drag = (event) => {
+      event.preventDefault()
+      pane.style.left = l + (event.pageX - startX) + 'px'
+      pane.style.top = t + (event.pageY - startY) + 'px'
+    }
+    
+    const mouseup = () => {
+      pane.classList.remove('is-dragging')
+      
+      document.removeEventListener('mousemove', drag)
+      document.removeEventListener('mouseup', mouseup)
+    }
+    
+    document.addEventListener("mousemove", drag)
+    document.addEventListener("mouseup", mouseup)
   })
   
 })
